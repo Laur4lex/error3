40f4e9ea776e47c969ca88a3b0770118dedda184
Laur4lex <103342663+Laur4lex@users.noreply.github.com>
2023-10-21 23:03:46 -0300

    Add files via upload

---
+++ tp5.js
@@ -0,0 +1,32 @@
+let estados;
+let fondos;
+let obstaculo;
+let mosca;
+function setup() {
+estados = new Pantallas();
+fondos = new Fondos();
+obstaculo = new Obstaculo();
+mosca = new Mosca();
+
+createCanvas(600, 600)
+}
+
+
+function draw() {
+ estados.dibujar();
+ 
+ }
+
+ function keyTyped() {
+    
+    if (key === 'r') {
+        mosca.reiniciar();
+      estados.reiniciar_();
+    } else if (key === 'c') {
+        mosca.reiniciar();
+      estados.Creditos();
+    }
+
+    
+
+  }
